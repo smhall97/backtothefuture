5d5aaf905f22ae4b95c9a8217e0ea7773f7a0b55
Siobhan <smhall97@gmail.com>
2020-07-25 09:41:02 +0200

    Initial commit

---
+++ logistic_regression.py
@@ -0,0 +1,4 @@
+"""
+Logistic regression implemented using the [scikitlearn framework](https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html)
+Aim: decode reward information (feedback) and responese (prediction) information within the neural signals
+"""
